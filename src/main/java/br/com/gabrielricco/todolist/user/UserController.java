7b5398e0d9a59909e9afbbe5ad5914f487d5d411
Gabriel Ricco <gabrielrdantas@outlook.com>
2023-10-15 19:18:31 -0300

    almost complete

--- src/main/java/br/com/gabrielricco/todolist/user/UserController.java
+++ src/main/java/br/com/gabrielricco/todolist/user/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 /*
  * Modificador
  * public
@@ -29,6 +31,9 @@ public class UserController {
       // Status code
       return ResponseEntity.status(400).body("Usuário já existe");
     }
+
+    var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+    userModel.setPassword(passwordHashred);
 
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(200).body(userCreated);
